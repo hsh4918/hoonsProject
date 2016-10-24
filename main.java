c6f5fddd24129f4019b4fa6c7c87560b2684cdf1
hsh4918 <hsh4918@naver.com>
2016-10-24 16:47:57 +0900

    Update main.java

--- main.java
+++ main.java
@@ -3,5 +3,6 @@ public class jaesung
   public static void main(string[] args)
   {
     system.out.println("open source hoons & sung & jae & hoon");
+    system.out.println("first open source sorry i'm lateing dionijaesung");
     }
 }
